b63626f1e888e3f9732850e53aec9405d1f2c239
rahulgirhepunje77 <rahulgirhepunje77@gmail.com>
2026-05-15 17:34:37 +0530

    Update variable.tf

--- Infra/variable.tf
+++ Infra/variable.tf
@@ -1,1 +1,23 @@
+variable "aws_region" {
+  default = "eu-west-1"
+}
 
+variable "environment" {
+  default = "lab"
+}
+
+variable "instance_type" {
+  default = "t2.nano"
+}
+
+variable "min_size" {
+  default = 1
+}
+
+variable "max_size" {
+  default = 2
+}
+
+variable "desired_capacity" {
+  default = 1
+}
